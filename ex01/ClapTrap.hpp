cc663764134f622e792d1574da5fe6adb527511a
Kate Sansom <katesansom@Kates-MacBook-Air.local>
2024-06-15 21:23:01 +0200

    ex00

---
+++ ex01/ClapTrap.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksansom <ksansom@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/14 13:59:36 by ksansom           #+#    #+#             */
+/*   Updated: 2024/06/14 14:34:24 by ksansom          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class ClapTrap {
+	public:
+		ClapTrap();
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap &copy);
+		ClapTrap &operator=(const ClapTrap &src);
+		~ClapTrap();
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+		
+	private:
+		std::string		_name;
+		unsigned int	_health;
+		unsigned int	_energy;
+		unsigned int	_damage;
+};
+
+#endif
